9ea05ef38665abd599b3777f9c2afdefec0c187c
SakataAtsuki <hhnkkr@icloud.com>
2023-04-02 15:37:44 +0900

    add main

---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"log"
+	"net/http"
+	"shoppingapp/pkg/usecase"
+)
+
+func main() {
+	serve()
+}
+
+func serve() {
+	r := gin.Default()
+
+	r.Static("/client", "./client")
+
+	r.StaticFS("/shoppingapp", http.Dir("./client/static"))
+
+	r.GET("/fetchAllProducts", usecase.ListProducts)
+
+	r.GET("/fetchProduct", usecase.GetProduct)
+
+	r.POST("/addProdut", usecase.CreateProduct)
+
+	r.POST("/changeStateProduct", usecase.UpdateProductState)
+
+	r.POST("/deleteProduct", usecase.DeleteProduct)
+
+	if err := r.Run(":8080"); err != nil {
+		log.Fatal("Server Run Failed.: ", err)
+	}
+}
